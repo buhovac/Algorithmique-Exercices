f2f837bdfd5b5af4dca78e44011bd55d5c7cc8b3
buhovac <marko.buhovac@yahoo.com>
2025-03-19 18:30:09 +0100

    Exercise 7.11. initial commit.

---
+++ exercise-7-11.py
@@ -0,0 +1,26 @@
+
+nombre_eleves = int(input("Nombre eleves? "))
+
+moyennes_eleves = []
+
+for i in range(nombre_eleves):
+    print(f"Élève {i + 1}")
+
+    nombre_notes = int(input(f"Combien de notes pour l'élève {i + 1} ? "))
+
+    notes = []
+
+    for j in range(nombre_notes):
+        note = float(input(f"Note {j + 1} : "))
+        notes.append(note)
+
+    moyenne = sum(notes) / nombre_notes
+    moyennes_eleves.append(moyenne)
+
+    print(f"Moyenne de l'élève {i + 1} : {moyenne:.2f}")
+
+moyenne_classe = sum(moyennes_eleves) / nombre_eleves
+
+
+print(f"Moyenne générale de la classe : {moyenne_classe:.2f}")
+
